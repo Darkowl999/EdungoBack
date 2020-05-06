4060a193a16bfb4205a06903d72df43aa7baf98d
RobertoDominguez <roberto_123_2000@hotmail.com>
2020-05-06 14:08:01 -0400

    Commit Inicial

---
+++ resources/views/login.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login edungo</title>
+</head>
+<body>
+
+    <center>
+
+    <input type="text" id="usuario" name="usuario" placeholder="nombre de usuario"><br><br>
+    <input type="password" id="password" name="password" placeholder="contraseña">
+    <br>
+    <br>
+    <button> iniciar sesion </button>
+    </center>
+
+
+</body>
+</html>
