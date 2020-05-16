6856f5ed9a7201a9c2331313b0ba7b2e3990338a
RobertoDominguez <roberto_123_2000@hotmail.com>
2020-05-16 16:43:46 -0400

    seedfinal

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,5 +13,6 @@ class DatabaseSeeder extends Seeder
     {
          $this->call(AdministradorSeeder::class);
          $this->call(ConfiguracionSeeder::class);
+         $this->call(DatosSeeder::class);
     }
 }
