17071ffb0548c27dfe65cf3dee567e1f6260f4cd
RobertoDominguez <roberto_123_2000@hotmail.com>
2020-05-16 16:12:21 -0400

    css

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -8,8 +8,8 @@
     <meta name="description" content="" />
     <meta name="author" content="" />
 
-    <link href="Template/css/styles.css" rel="stylesheet" />
-    <link href="https://cdn.datatables.net/1.10.20/css/dataTables.bootstrap4.min.css" rel="stylesheet" crossorigin="anonymous" />
+    <link href="{{ secure_asset('Template/css/styles.css') }}" rel="stylesheet" />
+    <link href="{{ secure_asset('https://cdn.datatables.net/1.10.20/css/dataTables.bootstrap4.min.css') }}" rel="stylesheet" crossorigin="anonymous" />
     <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/js/all.min.js" crossorigin="anonymous"></script>
 
 </head>
