4d4194b9aa00192cfc9909eed7e8d8fea4470cb4
RobertoDominguez <roberto_123_2000@hotmail.com>
2020-05-16 20:25:45 -0400

    s

--- app/Http/Controllers/ApiPersonaController.php
+++ app/Http/Controllers/ApiPersonaController.php
@@ -7,8 +7,6 @@ namespace App\Http\Controllers\Auth;
 use Illuminate\Http\Request;
 use App\Administrador;
 
-
-use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
