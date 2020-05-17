5053fd72d955fba43f7c8ce57f71d2b73971c9c0
RobertoDominguez <roberto_123_2000@hotmail.com>
2020-05-17 18:47:39 -0400

    s

--- app/Http/Controllers/Apis/ApiMateriaAuxiliarController.php
+++ app/Http/Controllers/Apis/ApiMateriaAuxiliarController.php
@@ -14,7 +14,8 @@ class ApiMateriaAuxiliarController extends Controller
     public function getAuxiliaresMateria(Request $request){
         $auxiliares=DB::table('materia')
         ->join('materia_auxiliar','materia.id','=','materia_auxiliar.id_materia')
-        ->join('auxiliar','auxiliar.id_persona','=','materia_auxiliar.id_auxiliar')->get();
+        ->join('auxiliar','auxiliar.id_persona','=','materia_auxiliar.id_auxiliar')
+        ->where('materia.id','=',$request->id_materia)->get();
 
         return response()->json($auxiliares,200);
     }
