5561761eaaf947a1aaf5bf222278971658bc90ed
RobertoDominguez <roberto_123_2000@hotmail.com>
2020-05-16 20:23:12 -0400

    s

--- app/Http/Controllers/ApiPersonaController.php
+++ app/Http/Controllers/ApiPersonaController.php
@@ -1,10 +1,13 @@
 <?php
 
-namespace App\Http\Controllers;
+//namespace App\Http\Controllers;
+
+namespace App\Http\Controllers\Auth;
+
 use Illuminate\Http\Request;
 use App\Administrador;
 
-namespace App\Http\Controllers\Auth;
+
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\RegistersUsers;
