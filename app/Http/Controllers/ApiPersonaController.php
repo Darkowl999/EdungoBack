31726a8d02e94c4b827625f7f21464028ec53fea
RobertoDominguez <roberto_123_2000@hotmail.com>
2020-05-16 20:15:42 -0400

    register

--- app/Http/Controllers/ApiPersonaController.php
+++ app/Http/Controllers/ApiPersonaController.php
@@ -1,30 +1,60 @@
 <?php
 
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
+use App\Administrador;
 
-use App\Persona;
+namespace App\Http\Controllers\Auth;
+
+use App\Providers\RouteServiceProvider;
+use Illuminate\Foundation\Auth\RegistersUsers;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
 
 class ApiPersonaController extends Controller
 {
-    public function __construct(){
+    use RegistersUsers;
 
+    public function __construct(){
+        $this->middleware('guest');
     }
 
     public function index(Request $request){
-        return Persona::all();
+        return Administrador::all();
     }
 
-    public function store(Request $request){
+   /* public function store(Request $request){
         return Persona::insert('insert into persona (nombre,apellido,nombre_usuario,telefono,sexo,email,direccion,fecha_nacimiento
             ,password) values (?,?,?,?,?,?,?,?,?)', [$request->nombre,$request->apellido,$request->nombre_usuario
             ,$request->telefono,$request->sexo,$request->email,$request->direccion,$request->fecha_nacimiento
             ,$request->password])->get();
+    }*/
+
+    public function store(Request $request)
+    {
+        $this->validator($request->all())->validate();
+
+        event(new Registered($user = $this->create($request->all())));
+
+        return $this->registered($request, $user) ? : 0;
     }
 
+
+    protected function validator(array $data)
+    {
+        return Validator::make($data, [
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:administrador'],
+            'password' => ['required', 'string', 'min:8', 'confirmed'],
+        ]);
+    }
     
-
-
+    
+    protected function create(array $data)
+    {
+        return Administrador::create([
+            'email' => $data['email'],
+            'password' => Hash::make($data['password']),
+        ]);
+    }
 
 }
