359696f539afc7bd6a86e89d2b7c67a3afcc165d
RobertoDominguez <roberto_123_2000@hotmail.com>
2020-05-16 23:36:39 -0400

    api

--- app/Http/Controllers/ApiPersonaController.php
+++ app/Http/Controllers/ApiPersonaController.php
@@ -16,7 +16,7 @@ class ApiPersonaController extends Controller
     }
 
     public function index(Request $request){
-        return Administrador::all();
+        return Persona::all();
     }
 
    /* public function store(Request $request){
@@ -25,33 +25,31 @@ class ApiPersonaController extends Controller
 
     public function store(Request $request)
     {
-
-
-      /*  
-        
-
-        */
-
-
        /* return Persona::insert('insert into persona (nombre,apellido,nombre_usuario,telefono,sexo,email,direccion,fecha_nacimiento
         ,password) values (?,?,?,?,?,?,?,?,?)', [$request->nombre,$request->apellido,$request->nombre_usuario
         ,$request->telefono,$request->sexo,$request->email,$request->direccion,$request->fecha_nacimiento
         ,$request->password]);
-    */
-        
-    $validatedData = $request->validate([
-        'email' => ['required', 'string', 'email', 'max:255', 'unique:administrador'],
-        'password' => ['required', 'string', 'min:8'],
-    ]);
-        if ($validatedData){
-             Administrador::insert([
-                'email' => $request['email'],
-                'password' => Hash::make($request['password']),
-            ]);
-            return "datos insertados correctamente";
-        }
-            return "error 500 :v ";
+        */
+        $email=Persona::find($request->email);
+        $nombre_usuario=Persona::find($request->nombre_usuario);
 
+        if (!is_null($email)){
+            return response()->json('Ya existe este email',500);
+        }
+        if (!is_null($nombre_usuario)){
+            return response()->json('Ya existe este nombre de usuario',500);  
+        }
+
+        if ($request->password!=$request->confirm_password){
+            return response()->json('La confirmacion de la contraseña es incorrecta',500);  
+        }
+
+        Persona::insert('insert into persona (nombre,apellido,nombre_usuario,telefono,sexo,email,direccion,fecha_nacimiento
+        ,password) values (?,?,?,?,?,?,?,?,?)', [$request->nombre,$request->apellido,$request->nombre_usuario
+        ,$request->telefono,$request->sexo,$request->email,$request->direccion,$request->fecha_nacimiento
+        ,$request->password]);
+
+            return response()->json('Datos insertados correctamente :v',200);
     }
 
 
