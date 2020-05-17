52374ffbd46143d0e540dcf708eb544b59983817
RobertoDominguez <roberto_123_2000@hotmail.com>
2020-05-17 16:55:18 -0400

    s

--- app/Http/Controllers/ApiEstudianteController.php
+++ app/Http/Controllers/ApiEstudianteController.php
@@ -13,7 +13,7 @@ class ApiEstudianteController extends Controller
         if (!is_null($persona)){
             $estudianteExists=Estudiante::where('id_persona','=',$request->id_persona)->first();
             if (is_null($estudianteExists)){
-                $estudiante=Estudiante::create($request->id_persona);
+                $estudiante=Estudiante::create($request);
                 return response()->json($estudiante,200);
             }else{
                 return response()->json($estudianteExists,200);
