b0701a0ad0af4d432fb08ca7a43f1cea63e457df
RobertoDominguez <roberto_123_2000@hotmail.com>
2020-05-20 19:10:00 -0400

    s

--- app/Http/Controllers/Apis/ApiMateriaAuxiliarController.php
+++ app/Http/Controllers/Apis/ApiMateriaAuxiliarController.php
@@ -12,13 +12,16 @@ use Illuminate\Support\Facades\DB;
 class ApiMateriaAuxiliarController extends Controller
 {
     public function getAuxiliaresMateria(Request $request){
+
+        $materia=Materia::find($request->id_materia);
+        if (is_null($materia)){
+            return response()->json('La materia de la peticion no existe',500) ;
+        }
         $auxiliares=DB::table('materia')
         ->join('materia_auxiliar','materia.id','=','materia_auxiliar.id_materia')
         ->join('auxiliar','auxiliar.id_persona','=','materia_auxiliar.id_auxiliar')
         ->where('materia.id','=',$request->id_materia)->get();
-
-        return (is_null($auxiliares))?  
-        response()->json('La materia de la peticion no existe',500) :
-        response()->json($auxiliares,200);  
+        
+        return response()->json($auxiliares,200);  
     }
 }
