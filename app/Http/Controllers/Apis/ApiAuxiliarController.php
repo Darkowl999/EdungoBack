a015c57f86ce329165adc0ddaaece37bb797ca36
RobertoDominguez <roberto_123_2000@hotmail.com>
2020-05-25 00:52:46 -0400

    s

--- app/Http/Controllers/Apis/ApiAuxiliarController.php
+++ app/Http/Controllers/Apis/ApiAuxiliarController.php
@@ -12,7 +12,7 @@ class ApiAuxiliarController extends Controller
 
     public function existeAuxiliar(Request $request){
 
-        $auxiliar=Auxiliar::find($request->id_persona)->get();
+        $auxiliar=Auxiliar::find($request->id_persona);
 
         return (is_null($auxiliar))? 
          response()->json('no existe el auxiliar',500)
